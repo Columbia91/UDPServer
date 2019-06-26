0dacde1a2c9eab2d97454108881692849755c043
Columbia91 <alpha.z91@mail.ru>
2019-06-26 21:49:11 +0600

    Добавьте файлы проекта.

---
+++ Client/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            string str = "Hello! Send me Time";
+            
+            string ipServer = "127.0.0.1";
+            int port = 12345;
+            EndPoint srvEP = new IPEndPoint(IPAddress.Parse(ipServer), port);
+
+            client.SendTo(Encoding.UTF8.GetBytes(str), srvEP);
+            //Console.WriteLine("Send size: {0}", recSize);
+            
+            byte[] buffer = new byte[1024];
+            int recSize = client.Receive(buffer);
+            string message = Encoding.UTF8.GetString(buffer, 0, recSize);
+            Console.WriteLine("Time for this moment: {0}", message);
+        }
+    }
+}
---
+++ UDPServer/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace UDPServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            serverSocket.Bind(new IPEndPoint(IPAddress.Any, 12345));
+
+            // начинаем прослушивание
+            
+            byte[] buffer = new byte[64 * 1024];
+            EndPoint clientEndPoint = new IPEndPoint(0, 0);
+            int recSize = serverSocket.ReceiveFrom(buffer, ref clientEndPoint);
+
+            Console.WriteLine(Encoding.UTF8.GetString(buffer, 0, recSize));
+
+            string time = DateTime.Now.ToShortTimeString();
+            buffer = Encoding.UTF8.GetBytes(time);
+            serverSocket.SendTo(buffer, clientEndPoint);
+        }
+    }
+}
